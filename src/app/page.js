133d47846459d5c74e55bd3489ebcac2620d6437
Alt5ConCre <150808511+Alt5ConCre@users.noreply.github.com>
2026-09-14 10:45:28 +0400

    Make exploding watch the main homepage experience

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,9 @@
-import Hero from '@/components/Hero';
-import HorologicalArt from '@/components/HorologicalArt';
-import ProductShowcase from '@/components/ProductShowcase';
-import Movement from '@/components/Movement';
-import TechnicalSpecs from '@/components/TechnicalSpecs';
-import FeaturesGallery from '@/components/FeaturesGallery';
-import SocialProof from '@/components/SocialProof';
-import Craftsmanship from '@/components/Craftsmanship';
-import Footer from '@/components/Footer';
+import ExplodingWatch from '@/components/ExplodingWatch';
 
 export default function Home() {
   return (
     <main className="smooth-wrapper">
-      <Hero />
-      <HorologicalArt />
-      <ProductShowcase />
-      <Movement />
-      <TechnicalSpecs />
-      <FeaturesGallery />
-      <SocialProof />
-      <Craftsmanship />
-      <Footer />
+      <ExplodingWatch />
     </main>
   );
 }
