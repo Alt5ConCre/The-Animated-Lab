6592fd9c72223a64f403aa294aea024796561035
Alt5ConCre <150808511+Alt5ConCre@users.noreply.github.com>
2026-09-14 12:15:52 +0400

    Reset homepage to new cinematic watch experience

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,7 @@
 'use client';
 
 import ExplodingWatch from '@/components/ExplodingWatch';
-import HorologicalArt from '@/components/HorologicalArt';
-import FeaturesGallery from '@/components/FeaturesGallery';
-import Craftsmanship from '@/components/Craftsmanship';
 
 export default function Home() {
-  return (
-    <main>
-      <ExplodingWatch />
-      <HorologicalArt />
-      <FeaturesGallery />
-      <Craftsmanship />
-    </main>
-  );
+  return <main><ExplodingWatch /></main>;
 }
