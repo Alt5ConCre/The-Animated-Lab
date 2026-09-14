2af075176df93f141d2e8281cb7f8f6b31da5d6b
Alt5ConCre <150808511+Alt5ConCre@users.noreply.github.com>
2026-09-14 11:40:37 +0400

    Assemble cinematic luxury watch experience from repo components

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,17 @@
 'use client';
 
 import ExplodingWatch from '@/components/ExplodingWatch';
+import HorologicalArt from '@/components/HorologicalArt';
+import FeaturesGallery from '@/components/FeaturesGallery';
+import Craftsmanship from '@/components/Craftsmanship';
 
 export default function Home() {
-  return <ExplodingWatch />;
+  return (
+    <main>
+      <ExplodingWatch />
+      <HorologicalArt />
+      <FeaturesGallery />
+      <Craftsmanship />
+    </main>
+  );
 }
